e5601b4250fc80c1fcc48037b7f0915eb0ec552b
Erkebulan Boltaikhan <146176928+spacecowboytobykty123@users.noreply.github.com>
2025-06-09 01:39:53 +0500

    posgres.go db retries

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"database/sql"
 	bckt "github.com/spacecowboytobykty123/bucketProto/gen/go/bucket"
+	"log"
 	"time"
 )
 
@@ -24,7 +25,24 @@ type StorageDetails struct {
 }
 
 func OpenDB(details StorageDetails) (*Storage, error) {
-	db, err := sql.Open("postgres", details.DSN)
+	var db *sql.DB
+	var err error
+	for i := 0; i < 10; i++ {
+		db, err = sql.Open("postgres", details.DSN)
+		if err == nil {
+			err = db.Ping()
+		}
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+		log.Printf("retrying DB connection... (%d/10)", i+1)
+	}
+
+	if err != nil {
+
+		log.Fatal("failed to connect to database after retries:", err)
+	}
 
 	if err != nil {
 		return nil, err
